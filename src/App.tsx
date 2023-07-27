f59a79cbb11949dd075f8b435158b2597eba43b8
Harkeerat Kanwal <hsk532991@gmail.com>
2023-07-27 11:36:52 -0400

    Add inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import Styles from "./App.module.css";
 import NewTransScreen from "./components/NewTransPage/NewTransScreen";
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 function App() {
   return (
-    <div className={Styles["app-container"]}>
-      <NewTransScreen />
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <div className={Styles["app-container"]}>
+        <NewTransScreen />
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/NewTransPage/NewTransScreen.tsx
+++ src/components/NewTransPage/NewTransScreen.tsx
@@ -1,11 +1,32 @@
+import { TextField } from "@mui/material";
 import Styles from "./NewTransScreen.module.css";
+import { useEffect, useState } from "react";
 
 function NewTransScreen() {
+  const [phrase, setPhrase] = useState("");
+  const [meaning, setMeaning] = useState("");
+
   return (
     <div className={Styles.screenContainer}>
-      <div className={Styles.header}>
+      <header className={Styles.header}>
         <h1 className={Styles.title}>BRONZELIST</h1>
         <h3 className={Styles.subtitle}>CREATE HEADLIST</h3>
+      </header>
+
+      <div className={Styles["inputs-container"]}>
+        <TextField
+          variant="filled"
+          label="Target Word or Phrase"
+          value={phrase}
+          onChange={(e) => setPhrase(e.target.value)}
+        />
+        <div className={Styles["input-spacer"]}></div>
+        <TextField
+          variant="filled"
+          label="Meaning"
+          value={meaning}
+          onChange={(e) => setMeaning(e.target.value)}
+        />
       </div>
     </div>
   );
