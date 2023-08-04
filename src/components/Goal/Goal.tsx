b5b592ba9c47d778262370849b8c76623f3a7406
Harkeerat Kanwal <hsk532991@gmail.com>
2023-08-04 15:31:03 -0400

    Fix bug where you could click complete goal

--- src/components/Goal/Goal.tsx
+++ src/components/Goal/Goal.tsx
@@ -20,12 +20,7 @@ function Goal(props: GoalProps) {
               d="M81.7,17.8C73.5,9.3,62,4,49.2,4 C24.3,4,4,24.3,4,49.2s20.3,45.2,45.2,45.2s45.2-20.3,45.2-45.2c0-8.6-2.4-16.6-6.5-23.4l0,0L45.6,68.2L24.7,47.3"
             />
           </svg>
-          <span
-            className={Styles["goal-text"] + " " + Styles["text-completed"]}
-            onClick={props.onClick}
-          >
-            {props.text}
-          </span>
+          <span className={Styles["goal-text"] + " " + Styles["text-completed"]}>{props.text}</span>
         </>
       ) : (
         <span
